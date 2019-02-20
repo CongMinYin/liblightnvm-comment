5c0d66d61bdafa402a75540001f3693caeccd9fe
cmyin <cmyin>
2019-02-20 17:18:03 +0800

    test large

--- tests/my_test_rw_spid.c
+++ tests/my_test_rw_spid.c
@@ -45,7 +45,7 @@ void ewr_s12_1addr(int use_meta)
 	//一个lun的plane数目乘以一个page的sector数目
 	const int naddrs = geo->nplanes * geo->nsectors;	
 	struct nvm_addr addrs[naddrs];
-	struct nvm_addr blk_addr = { .val = 0 };
+	struct nvm_addr blk_addr[32] = { 0 };
 	struct nvm_ret ret;
 	ssize_t res;
 	size_t buf_w_nbytes,  buf_r_nbytes;
@@ -53,14 +53,8 @@ void ewr_s12_1addr(int use_meta)
 	int failed = 1;
 	struct timeval start, end;
 
-	// 获得空闲地址blk_addr，获取的地址是块block
-	gettimeofday( &start, NULL );
-	if (nvm_cmd_gbbt_arbs(dev, NVM_BBT_FREE, 1, &blk_addr)) {
-		printf("nvm_cmd_gbbt_arbs");
-		goto out;
-	}
-	gettimeofday( &end, NULL );
-	printf("alloc block diff: %ld\n", (end.tv_sec - start.tv_sec) * 1000000 + end.tv_usec - start.tv_usec);
+	//gettimeofday( &end, NULL );
+	//printf("alloc block diff: %ld\n", (end.tv_sec - start.tv_sec) * 1000000 + end.tv_usec - start.tv_usec);
 
 	buf_w_nbytes = naddrs * geo->sector_nbytes;	//buf的字节数，
 	buf_r_nbytes = geo->sector_nbytes;	//每个扇区字节数，用于读
@@ -80,37 +74,44 @@ void ewr_s12_1addr(int use_meta)
 		goto out;
 	}
 
+	// 获得空闲地址blk_addr，获取的地址是块block
+	gettimeofday( &start, NULL );
+	for(int i = 0; i < 32; ++i){
 	// 地址赋值，此处应该是0，所以是else
 	if (pmode) {
-		addrs[0].ppa = blk_addr.ppa;
+		addrs[0].ppa = blk_addr[i].ppa;
 	} else {
 		for (size_t pl = 0; pl < geo->nplanes; ++pl) {
-			addrs[pl].ppa = blk_addr.ppa;
+			addrs[pl].ppa = blk_addr[i].ppa;
 
 			addrs[pl].g.pl = pl;
 		}
 	}
 
+	if (nvm_cmd_gbbt_arbs(dev, NVM_BBT_FREE, 1, &blk_addr[i])) {
+		printf("nvm_cmd_gbbt_arbs");
+		goto out;
+	}
 	// 先擦除，如果是获取的空闲块，在femu中应该不用擦除
-	gettimeofday( &start, NULL );
+	//gettimeofday( &start, NULL );
 	res = nvm_cmd_erase(dev, addrs, pmode ? 1 : geo->nplanes, NULL, pmode,
 			    &ret);
 	if (res < 0) {
 		printf("Erase failure");
 		goto out;
 	}
-	gettimeofday( &end, NULL );
-	printf("erase block diff: %ld\n", (end.tv_sec - start.tv_sec) * 1000000 + end.tv_usec - start.tv_usec);
+	//gettimeofday( &end, NULL );
+	//printf("erase block diff: %ld\n", (end.tv_sec - start.tv_sec) * 1000000 + end.tv_usec - start.tv_usec);
 
 	// 外层循环geo->npages是一个block的page数量
 	// 内层循环naddr是一个lun的plane数目乘以一个page的sector数目
 	// 最后的结果是写满1个lun，目的可能是并行，plane可以并行，并行度为一个lun的plane数
 	// 一次写命令写入naddr的扇区数目
 	// plane和lun的关系应该是横竖交叉的关系，都可以并行，并非包含
-	gettimeofday( &start, NULL );
+	//gettimeofday( &start, NULL );
 	for (size_t pg = 0; pg < geo->npages; ++pg) {
 		for (int i = 0; i < naddrs; ++i) {
-			addrs[i].ppa = blk_addr.ppa;
+			addrs[i].ppa = blk_addr[i].ppa;
 
 			addrs[i].g.pg = pg;
 			addrs[i].g.sec = i % geo->nsectors;
@@ -123,23 +124,19 @@ void ewr_s12_1addr(int use_meta)
 			goto out;
 		}
 	}
+	}
 	gettimeofday( &end, NULL );
-	printf("write size:%lu x %lu = %lu byte diff: %ld\n", geo->npages, buf_w_nbytes, geo->npages*buf_w_nbytes, (end.tv_sec - start.tv_sec) * 1000000 + end.tv_usec - start.tv_usec);
-
-		gettimeofday( &start, NULL );
-		res = nvm_cmd_write(dev, addrs, naddrs, buf_w,
-				     use_meta ? meta_w : NULL, pmode, &ret);
-    gettimeofday( &end, NULL );
-		printf("single write size:%lu diff: %ld\n", buf_w_nbytes, (end.tv_sec - start.tv_sec) * 1000000 + end.tv_usec - start.tv_usec);
+	printf("write size:%lu MB diff: %ld\n", 32*geo->npages*buf_w_nbytes/1024/1024, (end.tv_sec - start.tv_sec) * 1000000 + end.tv_usec - start.tv_usec);
 
   // 读取是三层循环，plane层循环放中间也是为了并行性
 	gettimeofday( &start, NULL );
+	for(int i = 0; i < 32; ++i){
 	for (size_t pg = 0; pg < geo->npages; ++pg) {
 		for (size_t pl = 0; pl < geo->nplanes; ++pl) {
 			for (size_t sec = 0; sec < geo->nsectors; ++sec) {
 				struct nvm_addr addr;
 
-				addr.ppa = blk_addr.ppa;
+				addr.ppa = blk_addr[i].ppa;
 				addr.g.pg = pg;
 				addr.g.pl = pl;
 				addr.g.sec = sec;
@@ -157,20 +154,10 @@ void ewr_s12_1addr(int use_meta)
 			}
 		}
 	}
+	}
 	gettimeofday( &end, NULL );
-	printf("read size:%lu x %lu x %lu x %lu = %lu byte diff: %ld\n", geo->npages, geo->nplanes,  geo->nsectors, buf_r_nbytes, geo->npages*geo->nplanes*geo->nsectors*buf_r_nbytes,  (end.tv_sec - start.tv_sec) * 1000000 + end.tv_usec - start.tv_usec);
+	printf("read size:%lu MB diff: %ld\n", 32*geo->npages*geo->nplanes*geo->nsectors*buf_r_nbytes/1024/1024,  (end.tv_sec - start.tv_sec) * 1000000 + end.tv_usec - start.tv_usec);
 
-				struct nvm_addr addr;
-
-				addr.ppa = blk_addr.ppa;
-				addr.g.pg = 0;
-				addr.g.pl = 0;
-				addr.g.sec = 0;
-		gettimeofday( &start, NULL );
-		res = nvm_cmd_read(dev, &addr, 1, buf_r,
-						    use_meta ? meta_r : NULL, pmode, &ret);
-    gettimeofday( &end, NULL );
-		printf("single read size:%lu diff: %ld\n", buf_r_nbytes, (end.tv_sec - start.tv_sec) * 1000000 + end.tv_usec - start.tv_usec);
 	failed = 0;
 	printf("Success\n");
   return ;
